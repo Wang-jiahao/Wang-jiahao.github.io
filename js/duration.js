f5894b800a5ae22267b02aaaaacf0c592e2602c5
wang-jiahao <2328796228@qq.com>
2025-01-10 12:18:29 +0800

    Site updated: 2025-01-10 12:18:27

--- js/duration.js
+++ js/duration.js
@@ -22,7 +22,7 @@
     if(String(snum).length === 1 ){
       snum = "0" + snum;
     }
-    document.getElementById("timeDate").innerHTML = "本站勉强运行&nbsp"+dnum+"&nbsp天";
+    document.getElementById("timeDate").innerHTML = "本站勉强维持🥹&nbsp"+dnum+"&nbsp天";
     document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
   }
 
